87533b8b1fb3798ea9a947a83409f0aaa7e2dc24
Rodrigo Martínez <dev@brunneis.com>
2017-06-26 19:47:55 +0200

    OpenJDK for build script

--- scripts/build-hadoop-centos.sh
+++ scripts/build-hadoop-centos.sh
@@ -23,14 +23,11 @@
 
 WORKING_DIR=$(pwd)
 
-# ORACLE JDK SCRIPT VARIABLES
-JDK_VERSION=1.8.0_112
-JDK_BIN_ARCHIVE=jdk-8u112-linux-x64.tar.gz
-JDK_DOWNLOAD_LINK=http://download.oracle.com/otn-pub/java/jdk/8u112-b15/$JDK_BIN_ARCHIVE
-JDK_INSTALL_DIR=/opt/oracle/java
+# JDK SCRIPT VARIABLES
+JDK_VERSION=1.8.0
 
 # APACHE MAVEN SCRIPT VARIABLES
-MAVEN_VERSION=3.3.9
+MAVEN_VERSION=3.5.0
 MAVEN_BIN_ARCHIVE=apache-maven-$MAVEN_VERSION-bin.tar.gz
 MAVEN_DOWNLOAD_LINK=http://apache.rediris.es/maven/maven-3/$MAVEN_VERSION/binaries/$MAVEN_BIN_ARCHIVE
 MAVEN_INSTALL_DIR=/opt/apache/maven
@@ -55,21 +52,10 @@ yum -y update && yum -y install wget \
 make gcc gcc-c++ gawk kernel-devel autoconf automake libtool cmake zlib-devel pkgconfig openssl-devel \
 snappy-devel bzip2 bzip2-devel jansson-devel fuse-devel 
 
-# ORACLE JDK INSTALL
+# OPENJDK JDK INSTALL
 echo -e 'Installing Oracle JDK...\n'
-mkdir -p $JDK_INSTALL_DIR
-cd $JDK_INSTALL_DIR
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-$JDK_DOWNLOAD_LINK
-tar xzf $JDK_BIN_ARCHIVE
-rm -f $JDK_BIN_ARCHIVE
-ln -sf jdk$JDK_VERSION current
-echo export JAVA_HOME=$JDK_INSTALL_DIR/current > /etc/profile.d/oracle-jdk.sh
-echo export JRE_HOME=$JDK_INSTALL_DIR/current/jre >> /etc/profile.d/oracle-jdk.sh
-echo 'if ! echo $PATH | grep -q '$JDK_INSTALL_DIR'/current/bin:'$JDK_INSTALL_DIR'/current/jre/bin ; then
-export PATH='$JDK_INSTALL_DIR'/current/bin:'$JDK_INSTALL_DIR'/current/jre/bin:$PATH ; fi' \
->> /etc/profile.d/oracle-jdk.sh
-. /etc/profile.d/oracle-jdk.sh
+yum -y install java-$JDK_VERSION-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-$JDK_VERSION
 
 # APACHE MAVEN
 echo -e 'Installing Maven...\n'
